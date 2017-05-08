eb851d97fda77c846a831b45b70f5e9fb73748c9
Statsenko Vladimir <vovchiksta@gmail.com>
2017-05-08 18:53:49 +0300

    HttpFoundation + nikic/FastRoute

--- app/public/index.php
+++ app/public/index.php
@@ -2,3 +2,5 @@
 declare(strict_types = 1);
 
 require __DIR__.'/../src/bootstrap.php';
+
+echo memory_get_usage();
--- app/src/bootstrap.php
+++ app/src/bootstrap.php
@@ -1,1 +1,47 @@
 <?php
+namespace Sample;
+
+use FastRoute\Dispatcher;
+use FastRoute\RouteCollector;
+use function FastRoute\simpleDispatcher;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$request = Request::createFromGlobals();
+
+$response = Response::create();
+//$content, Response::HTTP_OK, ['content-type' => 'text/html']
+
+$dispatcher = simpleDispatcher(function(RouteCollector $r) {
+    $routes = include('routes.php');
+    foreach ($routes as $route) {
+        $r->addRoute($route[0], $route[1], $route[2]);
+    }
+});
+
+$routeInfo = $dispatcher->dispatch($request->getMethod(), $request->getRequestUri());
+
+// http://sample-project/index.php?r=/another-route
+// $routeInfo = $dispatcher->dispatch($request->getMethod(), $request->query->get('r'));
+
+switch ($routeInfo[0]) {
+    case Dispatcher::NOT_FOUND:
+        $response->setContent('404 - Page not found');
+        $response->setStatusCode(404);
+        break;
+    case Dispatcher::METHOD_NOT_ALLOWED:
+        $response->setContent('405 - Method not allowed');
+        $response->setStatusCode(405);
+        break;
+    case Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        call_user_func($handler, $vars);
+        break;
+}
+
+$response->prepare($request);
+//echo round((memory_get_usage() / 1024), 2).'; ';
+$response->send();
